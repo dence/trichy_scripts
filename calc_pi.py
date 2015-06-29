b12b669c0a494440e5363795e5bba2cd38073491
dence <dence@genetics.utah.edu>
2015-06-29 11:36:12 -0600

    added calc_pi and get_allele_freqs scripts

--- calc_pi.py
+++ calc_pi.py
@@ -2,9 +2,59 @@
 #06/19/2015
 
 import sys
+import re
+
 
 VCF = open(sys.argv[1], 'r')
 
 pi_counts = {}
 sample_positions = []
-
+FORMAT_found = 0
+FORMAT_pos = 0
+chrom_pattern = re.compile("CHROM")
+hashtag_pattern = re.compile("^#")
+FORMAT_pattern = re.compile("FORMAT")
+for line in VCF:
+	line = line.rstrip('\n')
+	if(FORMAT_pattern.search(line)):
+		INFO_parts = re.split("\t", line)
+		for part in INFO_parts:
+			if(FORMAT_found == 1):
+				sample_positions.append(part)
+				pi_counts.setdefault(part, [0,0])
+			else:
+				if FORMAT_pattern.match(part):
+					FORMAT_found = 1
+					FORMAT_pos = INFO_parts.index("FORMAT") + 1
+	else:
+		if(FORMAT_found == 1):
+			varLineParts = re.split("\t", line)
+			for sample in pi_counts:
+				sample_pos = FORMAT_pos + sample_positions.index(sample)
+				sample_INFO = varLineParts[sample_pos]
+				ref_allele = varLineParts[3]
+				alt_allele = varLineParts[4]
+				if(sample_INFO != "./." and
+				(len(ref_allele) == 1 and len(alt_allele) == 1)): 
+					INFO_parts = re.split("\:", sample_INFO)
+					curr_GT = INFO_parts[0]
+					#print curr_GT
+					if(curr_GT == "0/0"):
+						GT_list = pi_counts[sample]
+						GT_list[1] = GT_list[1] + 1
+					else:
+						if(curr_GT == "0/1"):
+							GT_list = pi_counts[sample]
+							GT_list[1] = GT_list[1] + 1
+							GT_list[0] = GT_list[0] + 1
+						else:
+							GT_list = pi_counts[sample]
+							GT_list[1] = GT_list[1] + 1
+							GT_list[0] = GT_list[0] + 1
+					#print pi_counts
+for sample in pi_counts:
+	curr_pi_list = pi_counts[sample]
+	diffs = curr_pi_list[0]
+	positions = curr_pi_list[1]	
+	pi = float(curr_pi_list[0]) / float(curr_pi_list[1])
+	print sample + "\t" + str(diffs) + "\t" + str(positions) + "\t" + "{0:.4f}".format(pi)
--- get_allele_freqs.py
+++ get_allele_freqs.py
@@ -7,54 +7,53 @@ import re
 
 VCF = open(sys.argv[1], 'r')
 
-pi_counts = {}
-sample_positions = []
 FORMAT_found = 0
 FORMAT_pos = 0
 chrom_pattern = re.compile("CHROM")
 hashtag_pattern = re.compile("^#")
 FORMAT_pattern = re.compile("FORMAT")
+header_line="overall_freq"
 for line in VCF:
 	line = line.rstrip('\n')
 	if(FORMAT_pattern.search(line)):
 		INFO_parts = re.split("\t", line)
 		for part in INFO_parts:
 			if(FORMAT_found == 1):
-				sample_positions.append(part)
-				pi_counts.setdefault(part, [0,0])
+				header_line = header_line + "\t" + part	
 			else:
 				if FORMAT_pattern.match(part):
 					FORMAT_found = 1
 					FORMAT_pos = INFO_parts.index("FORMAT") + 1
+	
 	else:
 		if(FORMAT_found == 1):
 			varLineParts = re.split("\t", line)
-			for sample in pi_counts:
-				sample_pos = FORMAT_pos + sample_positions.index(sample)
-				sample_INFO = varLineParts[sample_pos]
-				ref_allele = varLineParts[3]
-				alt_allele = varLineParts[4]
-				if(sample_INFO != "./." and
-				(len(ref_allele) == 1 and len(alt_allele) == 1)): 
-					INFO_parts = re.split("\:", sample_INFO)
-					curr_GT = INFO_parts[0]
-					#print curr_GT
-					if(curr_GT == "0/0"):
-						GT_list = pi_counts[sample]
-						GT_list[1] = GT_list[1] + 1
+			ref_allele = varLineParts[3]
+			alt_allele = varLineParts[4]
+
+			if(len(ref_allele) == 1 and len(alt_allele) == 1):
+				curr_line = ""
+				overall_INFO=varLineParts[FORMAT_pos - 2]
+				overall_parts = re.split("\;", overall_INFO)
+				overall_freq=overall_parts[1]
+				overall_freq_parts=re.split("\=", overall_freq)
+				overall_freq=overall_freq_parts[1]
+				curr_line = curr_line + overall_freq + "\t"
+				sample_INFOs = varLineParts[FORMAT_pos:len(varLineParts)]
+				for sample_INFO in sample_INFOs:
+					if(sample_INFO == "./."):
+						curr_line = curr_line + "0\t"
 					else:
-						if(curr_GT == "0/1"):
-							GT_list = pi_counts[sample]
-							GT_list[1] = GT_list[1] + 1
-							GT_list[0] = GT_list[0] + 1
+						sample_parts = re.split("\:",sample_INFO)
+						allele_depth = sample_parts[1]
+						if(allele_depth != "."):
+							[ref_depth,alt_depth] = re.split("\,",allele_depth)
+							total=ref_depth + alt_depth
+							alt_freq = float(alt_depth) / float(total)
+							if(alt_freq > 0):
+								curr_line = curr_line + "{0:.4f}".format(alt_freq) + "\t"
+							else:
+								curr_line = curr_line + "0\t"
 						else:
-							GT_list = pi_counts[sample]
-							GT_list[1] = GT_list[1] + 1
-							GT_list[0] = GT_list[0] + 1
-					#print pi_counts
-for sample in pi_counts:
-	curr_pi_list = pi_counts[sample]
-	diffs = curr_pi_list[0]
-	positions = curr_pi_list[1]	
-	pi = float(curr_pi_list[0]) / float(curr_pi_list[1])
-	print sample + "\t" + str(diffs) + "\t" + str(positions) + "\t" + "{0:.4f}".format(pi)
+							curr_line = curr_line + "0\t"
+				print curr_line	

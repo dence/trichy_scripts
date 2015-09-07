6ab0b5889ef1a3a2526ba59c32d5eabddafeea4e
dence <dandence@gmail.com>
2015-09-07 14:31:53 -0600

    updated gather_est2genome

--- gather_est2genome_hits.py
+++ gather_est2genome_hits.py
@@ -58,23 +58,30 @@ def find_longest_est2genome_hit(Seq_obj_name, curr_dir):
 	curr_datastore_log = curr_dir + "/" + log_file 
 	
 	new_record = SeqRecord("",id="",name="",description = "")
-	longest_map = {}
 	if(os.path.exists(curr_datastore_log)):
-		log_handle = open(curr_datastore_log,'r')
+		log_handle = open(curr_datastore_log,'r')	
+
 		longest_hit = ""
 		longest_length = 0
+
 		for line in log_handle:
 			seqname = line.split("\t")[0]
 			if(re.search(Seq_obj_name,seqname) and re.search("FINISHED",line)):
 				seq_dir = line.split("\t")[1]
 				tmp_refseq = seq_dir.split("/")[len(seq_dir.split("/")) - 2].replace(".","%2E")#hardcoded this conversion
 				gff_file = curr_dir + "/" + seq_dir + "/" +  tmp_refseq + ".gff"
-				if(length_of_hit(gff_file) > longest_length):
-					longest_map[gff_file] = length_of_hit(gff_file)
+				if(has_est2genome_hit(gff_file) and length_of_hit(gff_file) > 0):
+					longest_length = length_of_hit(gff_file)
+					longest_hit = gff_file
+				else:
+					print "Couldn't find est2genome_hit for this sample\t" + Seq_obj_name + "\t" + curr_dir	
+			#else:
+			#	print "couldn't find finished contig for this sample:\t" + Seq_obj_name + "\t" + curr_dir
 		log_handle.close()
-
-		if(len(new_record.id) > 0 and has_est2genome_hit(longest_hit)):
+		if(longest_length > 0):
 			new_record = make_new_SeqRecord(longest_hit, curr_dir)
+	else:
+		print "couldn't find this datastore log:\t" + curr_datastore_log
 
 	return new_record
 
@@ -92,6 +99,8 @@ def make_new_SeqRecord(gff_file, curr_sample_dir):
 	hit_stop = 0
 	hit_strand = "+"
 	gff_handle = open(gff_file, 'r')
+	print "making a SeqRecord from this file:\t"
+	print gff_file
 	for gff_line in gff_handle:
 		if(re.search("\texpressed_sequence_match\t",gff_line)):
 			hit_SeqID = gff_line.split("\t")[0]
@@ -108,12 +117,15 @@ def make_new_SeqRecord(gff_file, curr_sample_dir):
 
 	fasta_file = re.sub("\.maker\.output",".fasta",curr_sample_dir)
 	fasta_handle = open(fasta_file,'r')
-	record_dict = SeqIO.to_dict(SeqIO.parse(fasta_handle,"fasta"))
-	hit_record = record_dict[hit_SeqID]
+	hit_record = ""
+	print "opening this file:\t" + fasta_file
+	for record in SeqIO.parse(fasta_handle,"fasta"):
+		if(record.id == hit_SeqID):
+			hit_record = record
 	
 	new_seq = hit_record.seq[hit_start - 1:hit_stop]
 	if(hit_strand == "-"):
-		new_seq = hit_record.seq[curr_start - 1:curr_stop].reverse_complement()
+		new_seq = hit_record.seq[hit_start - 1:hit_stop].reverse_complement()
 	
 	seqname = curr_sample_dir.split("_")[0] + "_" + hit_SeqID
 	return SeqRecord(new_seq,id=seqname,name=seqname,description="")
@@ -130,11 +142,7 @@ def length_of_hit(gff_file):
 			curr_start = int(gff_line.split("\t")[3])
 			curr_stop = int(gff_line.split("\t")[4])
 			curr_strand = gff_line.split("\t")[6]
-		
-			if(curr_strand == "+"):
-				length = curr_stop - curr_start
-			else:
-				length = curr_start - curr_stop
+			length = curr_stop - curr_start	
 			break
 	gff_handle.close()
 
@@ -145,36 +153,48 @@ def gather_and_align_est2genome_hits(command_duple):
 	Seq_obj = command_duple[0]
 	maker_directories = command_duple[1]		 				
 
-	new_filehandle = open(Seq_obj.id + ".est2genome.fasta",'w')		
+	est2genome_fasta = Seq_obj.id + ".est2genome.fasta"
+	new_filehandle = open(est2genome_fasta,'w')		
+	print "writing to this file:\t" + est2genome_fasta
 	
 	SeqIO.write(Seq_obj,new_filehandle,"fasta")
 
 	for curr_dir in maker_directories:
+		
+		#using the the output directory got confusing 
 		#est2genome_Seq_obj = find_longest_est2genome_hit(Seq_obj.id, "../" + curr_dir)
 		est2genome_Seq_obj = find_longest_est2genome_hit(Seq_obj.id, curr_dir)
 		
 		if(est2genome_Seq_obj.id != ""):
 			SeqIO.write(est2genome_Seq_obj, new_filehandle,"fasta")
 
-	new_filehandle.close()	
+	new_filehandle.close()
+	print "finished with this file:\t" + est2genome_fasta
+
+	#now align the new est2genome hits file
+	curr_align_out = Seq_obj.id + ".est2genome.aln"
+	clustalw2_string = "clustalw2 -INFILE=" + est2genome_fasta + " -align -type=DNA -outfile=" + curr_align_out
+	print "aligning with this command:\t" + clustalw2_string	
+	subprocess.call(clustalw2_string,shell=True)
+	print "finished_aligning this:\t" + curr_align_out
 
 parser = argparse.ArgumentParser(description="gather est2genome hits from maker output directories")
 parser.add_argument("RefSeq",type=str,help="fasta file of the reference sequences")
-parser.add_argument("output_directory",help="directory to be made and filled with alignment files")
+#parser.add_argument("output_directory",help="directory to be made and filled with alignment files")
 parser.add_argument("nprocs",type=int,help="number of processes to run concurrently")
 parser.add_argument("directories",nargs='+',type=str,help="list of maker output directories")
 args=parser.parse_args()
 
-if not os.path.exists(args.output_directory):
-	os.mkdir(args.output_directory)
+#if not os.path.exists(args.output_directory):
+#	os.mkdir(args.output_directory)
 #os.chdir(args.output_directory)
 
 #command_duples = make_command_duples("../" + args.RefSeq, args.directories)
 command_duples = make_command_duples(args.RefSeq, args.directories)
 
 pool = mp.Pool(processes=args.nprocs)
-map(gather_and_align_est2genome_hits, command_duples)
-#pool.map(gather_and_align_est2genome_hits, command_duples)
+#map(gather_and_align_est2genome_hits, command_duples)
+pool.map(gather_and_align_est2genome_hits, command_duples)
 
 
 

6fe2dcccd4f7ff2765f97ad9173fab5e0521fede
dence <dandence@gmail.com>
2017-05-24 11:47:00 -0600

    added get gene distance script

---
+++ get_gene_nearest_neighbor.py
@@ -0,0 +1,12 @@
+#Daniel Ence
+#May 24, 2017
+
+import os
+import sys
+
+#open a MAKER gff3 file. 
+#go through all the gene features
+#find their nearest neighbors
+#(out of genes or non-simple repeat(ie TE))
+#print out 3 columns 
+#gene tab nearest neighbor(TE or gene) tab distance
